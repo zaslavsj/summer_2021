f048b664bff93c5af958f8ce4696f36702fe8498
zaslavsj <zaslavsj@mcmaster.ca>
2021-05-14 20:16:50 -0400

    Changes to overall appearance of the script and order of code.

--- covid_fidelity_analysis.R
+++ covid_fidelity_analysis.R
@@ -1,4 +1,14 @@
+###############################################################################
+### COVID-19 Project: RSKC Analysis for Fidelty Scores
+### 
+### Rachel Kwan and Jonathan Zaslavsky
+### May 14, 2021
+###
+###############################################################################
+
+#####################################
 ## Relevant Packages
+#####################################
 # Load packages
 library(easypackages)
 library(tidyverse)
@@ -7,33 +17,38 @@ library(RSKC)
 library(simEd)
 library(httr)
 library(Rtsne)
+library(here)
 
+#####################################
 ## Import and Prepare Dataset
+#####################################
 # Load full dataset
-all_fidelity <- read.csv("~/Documents/R/Murphy_Lab/COVID-19/Data/ALL_Fidelity.csv")
+all_fidelity <- read.csv(here("Data", "ALL_Fidelity.csv"))
 
-# Define a vector containing the genes of interest.
+# Define a vector containing the five genes of interest.
 genes_subset <- c("ACE2",
                   "DPP4",
                   "TMPRSS2",
                   "NRP1",
                   "ITGB3")
 
+# Define a vector containing all SARS-CoV-2 related genes.
 genes_full_list <- c("ACE2", "DPP4", "ANPEP", "CD209", "ENPEP", "CLEC4G", 
-                     "CLEC4M", "CEACAM-1", "TMPRSS2", "TMPRSS11d", 
+                     "CLEC4M", "CEACAM-1", "TMPRSS2", "TMPRSS11d", "NRP1",
                      "CTSL", "ADAM17", "FURIN", "ITGA5", "ITGB3", "ITGA4",
                      "ITGB6", "ITGA7")
 
 # Filter full dataset to include only the genes of interest and drop the 
 # columns that correspond to "Entrez" and "Alias".
 fidelity_subset <- all_fidelity %>%
-  filter(Gene %in% genes_subset) %>% 
+  filter(Gene %in% genes_subset) %>% # Includes five genes
+#  filter(Gene %in% genes_full_list) %>% # Includes full list of genes
   select(-c(2,3)) 
 
 # Tidy the data frame by separating the previous columns into
 # two new columns for the brain region and cell subtype. Assign each of the
-# values to a new column for the fidelity scores. The tidy data has each
-# column correspond to a varaible and each observation is a new row.
+# values to a new column for the fidelity scores. The tidy data's
+# columns each correspond to a varaible and each observation is a new row.
 fidelity_subset.long <-  pivot_longer(fidelity_subset, cols = -Gene,
                                       names_to = c("Brain.Region", "Cell.Subtype"),
                                       names_sep = "_",
@@ -47,8 +62,11 @@ fidelity_subset.wide <- pivot_wider(fidelity_subset.long,
                                     values_from = "Fidelity",
                                     names_sep = "_")
 
-# Define a vector of brain regions of interest.
-regions <- c("FCX", "PCX", "OCX", "TCX", "STR", "DI", "HIP", "AMY", "CB")
+# Define a vector containing brain regions of interest. This list comes from the
+# Oldham Lab website (https://oldhamlab.ctec.ucsf.edu/). Note: 'SC' contains 
+# NA values.
+regions <- c("FCX", "PCX", "TCX", "LIM", "IN", "OCX","BF", "CLA", "AMY", "HIP",
+             "STR", "GP", "DI", "MID", "PON", "MED", "CB", "WM") #, "SC")
 
 # Choose only the desired brain regions from the data frame.
 fidelity_subset.wide2 <- fidelity_subset.wide %>% 
@@ -62,59 +80,36 @@ fidelity <- fidelity_subset.wide2[, colSums(is.na(fidelity_subset.wide2)) < nrow
 myFidelity <- fidelity %>% 
   column_to_rownames("Brain.Region")
 
-## Prepare a dataset containing all COVID-related genes
-# Drop the columns corresponding to Entrez and Alias.
-all_fidelity2 <- all_fidelity %>% 
-#  filter(Gene %in% genes_full_list) %>% # Include only genes from 'genes_full_list'
-  select(-c(2,3))
-
-all_fidelity2.long <- pivot_longer(all_fidelity2, cols = -Gene,
-                                   names_to = c("Brain.Region", "Cell.Subtype"),
-                                   names_sep = "_",
-                                   values_to = "Fidelity")
-
-all_fidelity2.wide <- pivot_wider(all_fidelity2.long, 
-                                  names_from = c("Gene", "Cell.Subtype"),
-                                  values_from = "Fidelity",
-                                  names_sep = "_")
-
-# Remove the last row corresponding to mean expression.
-all_fidelity2.wide2 <- all_fidelity2.wide[-22, ]
-
-# Drop columns containing missing values.
-fidelity_full <- all_fidelity2.wide2[, colSums(is.na(all_fidelity2.wide2)) < nrow(all_fidelity2.wide2)]
-# Remove row corresponding to 'ALL'.
-fidelity_full <- fidelity_full %>% 
-  filter(!(Brain.Region %in% "ALL"))
-
-# Use the brain regions to name the rows and remove the brain region column.
-myFidelity_full <- fidelity_full %>% 
-  data.frame(row.names = fidelity_full$Brain.Region) %>% 
-  select(-1)
-
-#### Exploratory Analysis
+#####################################
+## Exploratory Analysis
+#####################################
 # Use data only related to the FCX region and ACE2, TMPRSS2 genes.
 plot_df <- fidelity_subset.long %>% 
   filter(Gene == "ACE2" | Gene == "TMPRSS2") %>% 
   filter(Brain.Region == "FCX")
 
-# Rough plot to check that we can reproduce previously made plots.
+# Rough plot to check that we can reproduce previously made bar plots.
 (ggplot(plot_df, aes(x = Cell.Subtype, y = Fidelity, fill = Gene)) +
   geom_bar(stat = "identity", position = "dodge") +
   theme_minimal())
 
+#####################################
 ## tSNE
-# Use fidelity for tSNE; it has brain regions as a column, not row names
+#####################################
+# Use 'fidelity' data for tSNE, which lists the brain regions as a column and
+# not as row names.
+# Omit NA values from 'fidelity' data frame before tSNE, as it will otherwise 
+# not work.
 tsne <- Rtsne(as.matrix(fidelity[,2:ncol(fidelity)]), perplexity = 1)
 
-# tsne_out: the two dimensions and corresponding regions
+# 'tsne_out' includes the two dimensions and corresponding brain regions.
 tsne_out <- tsne$Y %>%
   data.frame(regions) %>%
   rename(Brain.Region = regions, V1 = X1, V2 = X2) # Rename columns
 
-
+#####################################
 ## RSKC
-
+#####################################
 while (T) {
   
   # Assign the values of 2, 4, 6 and 8 to 'clust_vect'.
